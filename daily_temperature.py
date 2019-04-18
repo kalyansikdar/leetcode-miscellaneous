8f1c9ef108ff4f76a990f5c44265c3bf4c0ae963
Kalyan Sikdar <kalyansikdar@users.noreply.github.com>
2019-04-18 13:19:04 -0500

    Create daily_temperature.py

---
+++ daily_temperature.py
@@ -0,0 +1,21 @@
+# Given a list of daily temperatures T, return a list such that, for each day in the input, tells you how many days you would have 
+# to wait until a warmer temperature. If there is no future day for which this is possible, put 0 instead.
+# For example, given the list of temperatures T = [73, 74, 75, 71, 69, 72, 76, 73], your output should be [1, 1, 4, 2, 1, 1, 0, 0].
+# Note: The length of temperatures will be in the range [1, 30000]. Each temperature will be an integer in the range [30, 100].
+
+# TLE
+def dailyTemperatures(T):
+    result = []
+    for idx, i in enumerate(T):
+        max_val = i
+        flag = 0
+        for j in range(idx, len(T)):
+            if T[j] > max_val:
+                result.append(j-idx)
+                flag = 1
+                break
+        
+        if not flag:
+            result.append(0)
+            
+    return result
